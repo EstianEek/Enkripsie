1b7ccaa770eb909ae813b45cad8292e93342e7f5
Estian Eek <eek.estian@yahoo.com>
2020-05-11 14:12:59 +0200

    Initial commit

---
+++ Encryption/Encrypt.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Encryption
+{
+    partial class Encrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.rbPhoto = new System.Windows.Forms.RadioButton();
+            this.rbTXT = new System.Windows.Forms.RadioButton();
+            this.rbRAR = new System.Windows.Forms.RadioButton();
+            this.gbED = new System.Windows.Forms.GroupBox();
+            this.rbDecrypt = new System.Windows.Forms.RadioButton();
+            this.rbEncrypt = new System.Windows.Forms.RadioButton();
+            this.gbED.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(63, 55);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "Choose file";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rbPhoto
+            // 
+            this.rbPhoto.AutoSize = true;
+            this.rbPhoto.Location = new System.Drawing.Point(68, 37);
+            this.rbPhoto.Name = "rbPhoto";
+            this.rbPhoto.Size = new System.Drawing.Size(53, 17);
+            this.rbPhoto.TabIndex = 1;
+            this.rbPhoto.TabStop = true;
+            this.rbPhoto.Text = "Photo";
+            this.rbPhoto.UseVisualStyleBackColor = true;
+            this.rbPhoto.CheckedChanged += new System.EventHandler(this.rbPhoto_CheckedChanged);
+            // 
+            // rbTXT
+            // 
+            this.rbTXT.AutoSize = true;
+            this.rbTXT.Location = new System.Drawing.Point(159, 37);
+            this.rbTXT.Name = "rbTXT";
+            this.rbTXT.Size = new System.Drawing.Size(65, 17);
+            this.rbTXT.TabIndex = 2;
+            this.rbTXT.TabStop = true;
+            this.rbTXT.Text = "Text File";
+            this.rbTXT.UseVisualStyleBackColor = true;
+            this.rbTXT.CheckedChanged += new System.EventHandler(this.rbTXT_CheckedChanged);
+            // 
+            // rbRAR
+            // 
+            this.rbRAR.AutoSize = true;
+            this.rbRAR.Location = new System.Drawing.Point(250, 37);
+            this.rbRAR.Name = "rbRAR";
+            this.rbRAR.Size = new System.Drawing.Size(62, 17);
+            this.rbRAR.TabIndex = 3;
+            this.rbRAR.TabStop = true;
+            this.rbRAR.Text = "Rar FIle";
+            this.rbRAR.UseVisualStyleBackColor = true;
+            // 
+            // gbED
+            // 
+            this.gbED.Controls.Add(this.rbDecrypt);
+            this.gbED.Controls.Add(this.rbEncrypt);
+            this.gbED.Controls.Add(this.btnChoose);
+            this.gbED.Location = new System.Drawing.Point(95, 124);
+            this.gbED.Name = "gbED";
+            this.gbED.Size = new System.Drawing.Size(217, 93);
+            this.gbED.TabIndex = 4;
+            this.gbED.TabStop = false;
+            // 
+            // rbDecrypt
+            // 
+            this.rbDecrypt.AutoSize = true;
+            this.rbDecrypt.Location = new System.Drawing.Point(116, 19);
+            this.rbDecrypt.Name = "rbDecrypt";
+            this.rbDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.rbDecrypt.TabIndex = 2;
+            this.rbDecrypt.TabStop = true;
+            this.rbDecrypt.Text = "Decrypt";
+            this.rbDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // rbEncrypt
+            // 
+            this.rbEncrypt.AutoSize = true;
+            this.rbEncrypt.Location = new System.Drawing.Point(16, 19);
+            this.rbEncrypt.Name = "rbEncrypt";
+            this.rbEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.rbEncrypt.TabIndex = 1;
+            this.rbEncrypt.TabStop = true;
+            this.rbEncrypt.Text = "Encrypt";
+            this.rbEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // Encrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbED);
+            this.Controls.Add(this.rbRAR);
+            this.Controls.Add(this.rbTXT);
+            this.Controls.Add(this.rbPhoto);
+            this.Name = "Encrypt";
+            this.Text = "Encrypt";
+            this.gbED.ResumeLayout(false);
+            this.gbED.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.RadioButton rbPhoto;
+        private System.Windows.Forms.RadioButton rbTXT;
+        private System.Windows.Forms.RadioButton rbRAR;
+        private System.Windows.Forms.GroupBox gbED;
+        private System.Windows.Forms.RadioButton rbDecrypt;
+        private System.Windows.Forms.RadioButton rbEncrypt;
+    }
+}
---
+++ Encryption/Encrypt.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Encryption
+{
+    public partial class Encrypt : Form
+    {
+        byte[] fileKey;
+        byte[] fileIV;
+        public Encrypt()
+        {
+            InitializeComponent();
+        }
+
+        public byte[] ImageToByte(System.Drawing.Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        public Image ByteToImage(byte[] byteArray)
+        {
+            using (var ms = new MemoryStream(byteArray))
+            {
+                Image returnImage = Image.FromStream(ms);
+                return returnImage;
+            }
+        }
+        // funksie om foto te enkripteer
+        private void EncryptPicture(byte[] key, byte[] IV)
+        {
+            if (key == null || key.Length <= 0)
+            {
+                throw new ArgumentNullException("Key");
+            }
+            if (IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException("IV");
+            }
+
+            try
+            {
+                Image newImage;
+
+                openFile.ShowDialog();
+
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    newImage = Image.FromFile(openFile.FileName);
+                    string fileName = openFile.FileName;
+                    MessageBox.Show(fileName);
+
+                    byte[] image = ImageToByte(newImage);
+                    MessageBox.Show(Encoding.Default.GetString(image.ToArray()));
+
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        aesAlg.Key = key;
+                        aesAlg.IV = IV;
+
+                        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                        using (MemoryStream msEncrypt = new MemoryStream())
+                        {
+                            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                                {
+                                    swEncrypt.Write(image);
+                                    MessageBox.Show("Yes");
+                                    newImage = ByteToImage(msEncrypt.ToArray());
+                                    MessageBox.Show("Test");
+                                }
+
+                                
+                                
+
+                            }
+
+                            newImage.Save(fileName);
+                            MessageBox.Show("Image encrypted");
+                        }
+                    }
+
+                    MessageBox.Show("Encryption Done");
+
+                }
+                else
+                {
+                    MessageBox.Show("Cannot open file");
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Operation canceled");
+            }
+        }
+
+
+        // funksie om textfile te enkripteer
+        private void EncryptFile(byte[] key, byte[] IV)
+        {
+            MessageBox.Show(fileKey + " " + fileIV);
+
+            if (key == null || key.Length <= 0)
+            {
+                throw new ArgumentNullException("Key");
+            }
+            if(IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException("IV");
+            }
+
+            
+
+            try
+            {
+                StreamReader inputFile;
+                
+                openFile.ShowDialog();
+
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+                    string fileName = openFile.FileName;
+
+                    string text = inputFile.ReadToEnd();
+
+                    inputFile.Close();
+
+                    MessageBox.Show(text);
+
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        aesAlg.Key = key;
+                        aesAlg.IV = IV;
+
+                        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                        using (MemoryStream msEncrypt = new MemoryStream())
+                        {
+                            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                                {
+                                    swEncrypt.Write(text);
+                                }
+                                
+                                
+                                StreamWriter outputFile;
+                                outputFile = File.CreateText(fileName);
+                                outputFile.Write(Encoding.Default.GetString(msEncrypt.ToArray()));
+                                outputFile.Close();
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Encryption Done");
+
+                }
+                else
+                {
+                    MessageBox.Show("Cannot open file");
+                }
+
+               
+            }
+            catch
+            {
+                MessageBox.Show("Operation canceled");
+            }
+
+            
+        }
+
+
+        // funksie om textfile te dekripteer
+        private void DecryptFile(byte[] key, byte[] IV)
+        {
+            MessageBox.Show(fileKey + " " + fileIV);
+            if (key == null || key.Length <= 0)
+            {
+                throw new ArgumentNullException("Key");
+            }
+            if (IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException("IV");
+            }
+
+            try
+            {
+                StreamReader inputFile;
+
+                openFile.ShowDialog();
+
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+                    string fileName = openFile.FileName;
+                    MessageBox.Show(fileName);
+
+                    string text = inputFile.ReadToEnd();
+
+                    inputFile.Close();
+
+                    MessageBox.Show(text);
+
+                    byte[] cipher = Encoding.ASCII.GetBytes(text);
+                    MessageBox.Show(Encoding.Default.GetString(cipher.ToArray()));
+
+                    using (Aes aesAlg = Aes.Create())
+                    {
+                        aesAlg.Key = key;
+                        aesAlg.IV = IV;
+
+                        ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                        using (MemoryStream msDecrypt = new MemoryStream(cipher))
+                        {
+                            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                            {
+                                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                                {
+                                    text = srDecrypt.ReadToEnd();
+                                    MessageBox.Show(text);
+                                    
+                                }
+                                StreamWriter outputFile;
+                                MessageBox.Show("Test");
+                                outputFile = File.CreateText(fileName);
+
+                                outputFile.Write(text);
+
+                                outputFile.Close();
+                            }
+                        }
+                    }
+                }
+            }           
+            catch
+            {
+                MessageBox.Show("Operation cancelled");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            if(rbPhoto.Checked)
+            {
+                if(rbEncrypt.Checked)
+                {
+                    EncryptPicture(fileKey, fileIV);
+                }
+            }
+
+            else if(rbTXT.Checked)
+            {
+                if (rbEncrypt.Checked)
+                {
+                    EncryptFile(fileKey, fileIV);
+                }
+                else if (rbDecrypt.Checked)
+                {
+                    DecryptFile(fileKey, fileIV);
+                }
+            }
+            
+        }
+
+        private void rbTXT_CheckedChanged(object sender, EventArgs e)
+        {
+            using (Aes myAes = Aes.Create())
+            {
+
+                fileKey = myAes.Key;
+                fileIV = myAes.IV;
+
+            }
+        }
+
+        private void rbPhoto_CheckedChanged(object sender, EventArgs e)
+        {
+            using (Aes myAes = Aes.Create())
+            {
+
+                fileKey = myAes.Key;
+                fileIV = myAes.IV;
+
+            }
+        }
+    }
+}
